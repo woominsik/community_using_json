2933625d5e081b8bc8f4941b09794d593f286ce9
woominsik <9737638@naver.com>
2022-07-28 09:54:48 +0900

    이전 리포지터리에서 복사해서 시작

---
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -0,0 +1,127 @@
+package com.ll.exam.article;
+
+import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+import jakarta.servlet.ServletException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleController {
+
+    private ArticleService articleService;
+
+    public ArticleController() {
+        articleService = new ArticleService();
+    }
+
+
+    public void showList(Rq rq){
+        List<ArticleDto> articleDtos = articleDtos = articleService.findAll();
+
+        rq.setAttr("articles", articleDtos);
+        rq.view("usr/article/list");
+    }
+
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
+
+    public void doWrite(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        if (title.length() == 0) {
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+        long id = articleService.write(title, body);
+
+        rq.replace("/usr/article/detail/free/%d".formatted(id), "%d번 게시물이 생성 되었습니다.".formatted(id));
+    }
+
+    public void showDetail(Rq rq){
+        long id =rq.getLongPathValueByIndex(1,0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.findById(id);
+
+        if (articleDto == null) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+        rq.setAttr("article",articleDto);
+        rq.view("/usr/article/detail");
+    }
+
+    public void showModify(Rq rq) {
+        long id = rq.getLongPathValueByIndex(1,0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+        ArticleDto articleDto = articleService.findById(id);
+
+        if (articleDto == null) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        rq.setAttr("article",articleDto);
+        rq.view("/usr/article/modify");
+    }
+
+    public void doDelete(Rq rq) {
+        long id = rq.getLongPathValueByIndex(1, 0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.findById(id);
+
+        if (articleDto == null) {
+            rq.historyBack("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        articleService.delete(id);
+
+        rq.replace("/usr/article/list/free", "%d번 게시물이 삭제되었습니다.".formatted(id));
+    }
+
+    public void doModify(Rq rq) {
+        long id = rq.getLongPathValueByIndex(1, 0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.findById(id);
+
+        if (articleDto == null) {
+            rq.historyBack("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        articleService.modify(id, title, body);
+
+        rq.replace("/usr/article/detail/free/%d".formatted(id), "%d번 게시물이 수정되었습니다.".formatted(id));
+    }
+}
---
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -0,0 +1,18 @@
+package com.ll.exam.article.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+public class ArticleDto {
+    private long id;
+
+    private String title;
+
+    private String body;
+}
