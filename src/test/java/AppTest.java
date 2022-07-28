d03b2f5051fc27f411fb241d988b9c186ac1b2af
woominsik <9737638@naver.com>
2022-07-28 10:15:24 +0900

    잭슨으로 객체를 json으로 변경

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,5 +10,16 @@ public class AppTest {
     void 테스트_assertThat() {
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    void 실험_ObjectMapper() throws JsonProcessingException {
+        ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
+
+        String jsonStr = Ut.json.toJsonStr(articleDto, "");
+        assertThat(jsonStr).isNotBlank();
+        assertThat(jsonStr).isEqualTo("""
+                {"id":1,"title":"제목","body":"내용"}
+                """.trim());
     }
 }
